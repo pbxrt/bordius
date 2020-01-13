c0943bc3fd74208ebda53f797244a71aeeed3f71
PengYang <953413537@qq.com>
2020-01-13 16:28:52 +0800

    给 Home 页面添加样式

---
+++ src/application/Recommend/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Recommend() {
+    return <>recommend</>;
+}
+
+export default Recommend;
---
+++ src/application/Singers/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Singers() {
+    return (
+        <>
+            <h1>singers</h1>
+        </>
+    );
+}
+
+export default Singers;
