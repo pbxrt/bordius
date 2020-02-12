dceaa6bc598e5ef5420ff546898a593cbd298355
plearner <953413537@qq.com>
2020-02-12 23:25:46 +0800

    singers 页面增加数据层

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,39 +1,136 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import {categoryTypes, alphaTypes} from '../../api/config';
-import { NavContainer } from './style';
+import { NavContainer, ListContainer, List, ListItem } from './style';
+import Scroll from '../../baseUI/scroll';
+import {connect} from 'react-redux';
+import {
+    getSingerList,
+    getHotSingerList,
+    changeEnterLoading,
+    changePageCount,
+    refreshMoreSingerList,
+    changePullUpLoading,
+    changePullDownLoading,
+    refreshMoreHotSingerList,
+    changeCategory,
+    changeAlpha
+} from './store/actionCreators';
 
+// const singersList = [1,2,3,4,5,6,7,8,9,10,11,12].map(item => {
+//     return {
+//         picUrl: 'https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg',
+//         name: 'Linko',
+//         accountId: 8379565
+//     }
+// });
 
-function Singers() {
-    const [category, setCategory] = useState('');
-    const [alpha, setAlpha] = useState('');
-    function handleUpdateCategory(val) {
-        setCategory(val);
-    }
-
-    function handleUpdateAlpha(val) {
-        setAlpha(val);
-    }
-
+const renderSingerList = (singersList = []) => {
     return (
-        <NavContainer>
-            <Horizen
-                list={categoryTypes}
-                title={"分类（默认热门）："}
-                oldVal={category}
-                handleClick={handleUpdateCategory}>
-            </Horizen>
-            <Horizen
-                list={alphaTypes}
-                title={"首字母："}
-                oldVal={alpha}
-                handleClick={handleUpdateAlpha}>
-            </Horizen>
-        </NavContainer>
+        <List>
+            {
+                singersList.map((item, index) => {
+                    return (
+                        <ListItem key={item.accountId + '' + index}>
+                            <div className="img_wrapper">
+                                <img src={`${item.picUrl}?param=300x300`} width="100%" height="100%" alt="music" />
+                            </div>
+                            <span className="name">{item.name}</span>
+                        </ListItem>
+                    )
+                })
+            }
+        </List>
     );
 }
 
+function Singers(props) {
+    const {category, alpha, singerList} = props;
+    const {
+        getHotSingerDispatch,
+        updateDispatch,
+        pullUpRefreshDispatch,
+        pullDownRefreshDispatch
+    } = props;
+    
+    function handleUpdateCategory(val) {
+        updateDispatch(val, alpha);
+    }
 
+    function handleUpdateAlpha(val) {
+        updateDispatch(category, val);
+    }
 
-export default React.memo(Singers);
+    useEffect(() => {
+        getHotSingerDispatch();
+    }, []);
 
+    return (
+        <div>
+            <NavContainer>
+                <Horizen
+                    list={categoryTypes}
+                    title={"分类（默认热门）："}
+                    oldVal={category}
+                    handleClick={handleUpdateCategory}>
+                </Horizen>
+                <Horizen
+                    list={alphaTypes}
+                    title={"首字母："}
+                    oldVal={alpha}
+                    handleClick={handleUpdateAlpha}>
+                </Horizen>
+            </NavContainer>
+            <ListContainer>
+                <Scroll>
+                    {renderSingerList(singerList.toJS())}
+                </Scroll>
+            </ListContainer>
+        </div>
+    );
+}
+
+const mapStateToProps = state => {
+    return {
+        singerList: state.getIn(['singers', 'singerList']),
+        enterLoading: state.getIn(['singers', 'enterLoading']),
+        pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+        pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+        pageCount: state.getIn(['singers', 'pageCount']),
+        category: state.getIn(['singers', 'category']),
+        alpha: state.getIn(['singers', 'alpha'])
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    getHotSingerDispatch() {
+        dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+        dispatch(changeCategory(category));
+        dispatch(changeAlpha(alpha));
+        dispatch(changePageCount(0));
+        dispatch(changeEnterLoading(true));
+        dispatch(getSingerList(category, alpha));
+    },
+    pullUpRefreshDispatch(category, alpha, hot, count) {
+        dispatch(changePullUpLoading(true));
+        dispatch(changePageCount(count + 1));
+        if (hot) {
+            dispatch(refreshMoreHotSingerList());
+        } else {
+            dispatch(refreshMoreSingerList(category, alpha));
+        }
+    },
+    pullDownRefreshDispatch(category, alpha) {
+        dispatch(changePullDownLoading(true));
+        dispatch(changePageCount(0));
+        if (category === '' || alpha === '') {
+            dispatch(getHotSingerList());
+        } else {
+            dispatch(getSingerList(category, alpha));
+        }
+    }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers));
---
+++ src/application/Singers/store/reducer.js
@@ -0,0 +1,33 @@
+import {fromJS} from 'immutable';
+import * as actionTypes from './constants';
+
+const defaultState = fromJS({
+    singerList: [],
+    enterLoading: true,
+    pullUpLoading: false,
+    pullDownLoading: false,
+    pageCount: 0,
+    category: '',
+    alpha: ''
+});
+
+export default (state = defaultState, action) => {
+    switch(action.type) {
+        case actionTypes.CHANGE_SINGER_LIST:
+            return state.set('singerList', action.data);
+        case actionTypes.CHANGE_PAGE_COUNT:
+            return state.set('pageCount', action.data);
+        case actionTypes.CHANGE_ENTER_LOADING:
+            return state.set('enterLoading', action.data);
+        case actionTypes.CHANGE_PULLUP_LOADING:
+            return state.set('pullUpLoading', action.data);
+        case actionTypes.CHANGE_PULLDOWN_LOADING:
+            return state.set('pullDownLoading', action.data);
+        case actionTypes.CHANGE_CATEGORY:
+            return state.set('category', action.data);
+        case actionTypes.CHANGE_ALPHA:
+            return state.set('alpha', action.data);
+        default:
+            return state;
+    }
+}
