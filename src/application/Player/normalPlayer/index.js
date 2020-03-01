2b2828a39b0def1e096e6a26222ea3557bb84561
plearner <953413537@qq.com>
2020-03-01 12:45:55 +0800

    添加 progress-bar 组件

--- src/application/Player/normalPlayer/index.js
+++ src/application/Player/normalPlayer/index.js
@@ -6,10 +6,12 @@ import {
     Middle,
     Bottom,
     Operators,
-    CDWrapper
+    CDWrapper,
+    ProgressWrapper
 } from './style';
 import {CSSTransition} from 'react-transition-group';
 import animations from 'create-keyframe-animation';
+import ProgressBar from "../../../baseUI/progress-bar/index";
 
 const transform = prefixStyle('transform');
 
@@ -118,6 +120,13 @@ function NormalPlayer(props) {
                     </CDWrapper>
                 </Middle>
                 <Bottom className="bottom">
+                    <ProgressWrapper>
+                        <span className="time time-l">0:00</span>
+                        <div className="progress-bar-wrapper">
+                            <ProgressBar percent={0.2}></ProgressBar>
+                        </div>
+                        <div className="time time-r">4:17</div>
+                    </ProgressWrapper>
                     <Operators>
                         <div className="icon i-left" >
                             <i className="iconfont">&#xe625;</i>
---
+++ src/baseUI/progress-bar/index.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useRef, useState } from 'react';
+import styled from'styled-components';
+import style from '../../assets/global-style';
+import { prefixStyle } from './../../api/utils';
+
+const ProgressBarWrapper = styled.div`
+    height: 30px;
+    .bar-inner {
+        position: relative;
+        top: 13px;
+        height: 4px;
+        background: rgba(0, 0, 0, .3);
+        .progress {
+            position: absolute;
+            height: 100%;
+            background: ${style["theme-color"]};
+        }
+        .progress-btn-wrapper {
+            margin-left: 7px;
+            position: absolute;
+            left: -15px;
+            top: -13px;
+            width: 30px;
+            height: 30px;
+            .progress-btn {
+                position: relative;
+                top: 7px;
+                left: 7px;
+                box-sizing: border-box;
+                width: 16px;
+                height: 16px;
+                border: 3px solid ${style["border-color"]};
+                border-radius: 50%;
+                background: ${style["theme-color"]};
+            }
+    }
+  }
+`
+
+function ProgressBar(props) {
+    return (
+        <ProgressBarWrapper>
+            <div className="bar-inner">
+                <div className="progress"></div>
+                <div className="progress-btn-wrapper">
+                    <div className="progress-btn"></div>
+                </div>
+            </div>
+        </ProgressBarWrapper>
+    );
+}
+
+export default ProgressBar
