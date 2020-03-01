293cd38fdea6b76696ba7c1920427e124b89e1e6
plearner <953413537@qq.com>
2020-03-01 21:48:02 +0800

    使用mock数据完善交互逻辑，添加audio元素播放背景音乐

--- src/api/utils.js
+++ src/api/utils.js
@@ -95,3 +95,7 @@ export function prefixStyle(style) {
     }
     return vendor + style.charAt(0).toUpperCase() + style.substr(1);
 }
+
+export const getSongUrl = id => {
+    return `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
+};
--- src/application/Player/index.js
+++ src/application/Player/index.js
@@ -2,6 +2,7 @@ import React, {useRef, useState, useEffect} from 'react';
 import {connect} from 'react-redux';
 import MiniPlayer from './miniPlayer';
 import NormalPlayer from './normalPlayer';
+import {getSongUrl, isEmptyObject} from '../../api/utils';
 import {
     changePlayingState,
     changeShowPlayList,
@@ -13,29 +14,133 @@ import {
 } from './store';
 
 function Player(props) {
-    const {fullScreen, playing} = props;
-    const {toggleFullScreenDispatch, togglePlayingDispatch} = props;
-    const song = {
-        ar: [{name: "薛之谦"}],
-        name: '测试',
-        al: {
-            picUrl: 'http://p2.music.126.net/OuL80LuI347696oR98b3SA==/109951164738867906.jpg'
+    const {fullScreen, playing, currentIndex, currentSong: immutableCurrentSong} = props;
+    const {toggleFullScreenDispatch, togglePlayingDispatch, changeCurrentIndexDispatch, changeCurrentDispatch} = props;
+
+    let currentSong = immutableCurrentSong.toJS();
+    // 目前播放时间
+    const [currentTime, setCurrentTime] = useState(0);
+
+    // 歌曲总时长
+    const [duration, setDuration] = useState(0);
+
+    // 歌曲播放进度
+    let percent = (currentTime / duration) || 0;
+
+    const audioRef = useRef();
+
+    const playList = [
+    {
+      ftype: 0,
+      djId: 0,
+      a: null,
+      cd: '01',
+      crbt: null,
+      no: 1,
+      st: 0,
+      rt: '',
+      cf: '',
+      alia: [
+        '手游《梦幻花园》苏州园林版推广曲'
+      ],
+      rtUrls: [],
+      fee: 0,
+      s_id: 0,
+      copyright: 0,
+      h: {
+        br: 320000,
+        fid: 0,
+        size: 9400365,
+        vd: -45814
+      },
+      mv: 0,
+      al: {
+        id: 84991301,
+        name: '拾梦纪',
+        picUrl: 'http://p1.music.126.net/M19SOoRMkcHmJvmGflXjXQ==/109951164627180052.jpg',
+        tns: [],
+        pic_str: '109951164627180052',
+        pic: 109951164627180050
+      },
+      name: '拾梦纪',
+      l: {
+        br: 128000,
+        fid: 0,
+        size: 3760173,
+        vd: -41672
+      },
+      rtype: 0,
+      m: {
+        br: 192000,
+        fid: 0,
+        size: 5640237,
+        vd: -43277
+      },
+      cp: 1416668,
+      mark: 0,
+      rtUrl: null,
+      mst: 9,
+      dt: 234947,
+      ar: [
+        {
+          id: 12084589,
+          name: '妖扬',
+          tns: [],
+          alias: []
+        },
+        {
+          id: 12578371,
+          name: '金天',
+          tns: [],
+          alias: []
         }
-    };
+      ],
+      pop: 5,
+      pst: 0,
+      t: 0,
+      v: 3,
+      id: 1416767593,
+      publishTime: 0,
+      rurl: null
+    }
+];
+
+    useEffect(() => {
+        if (!currentSong) return;
+        changeCurrentIndexDispatch(0);
+        let current = playList[0];
+        changeCurrentDispatch(current);
+        audioRef.current.src = getSongUrl(current.id);
+        setTimeout(() => {
+            audioRef.current.play();
+        })
+        console.log(audioRef.current.src);
+        togglePlayingDispatch(true);
+        setCurrentTime(0);
+        setDuration((current.dt / 1000) | 0);
+    }, []);
+
     return (
         <div className="players-container">
-            <MiniPlayer
-                song={song}
-                fullScreen={fullScreen}
-                toggleFullScreen={toggleFullScreenDispatch}
-                playing={playing}
-                clickPlaying={togglePlayingDispatch}
-            />
-            <NormalPlayer
-                song={song}
-                fullScreen={fullScreen}
-                toggleFullScreen={toggleFullScreenDispatch}  
-            />
+            {
+                !isEmptyObject(currentSong) && (
+                    <>
+                        <MiniPlayer
+                            song={currentSong}
+                            fullScreen={fullScreen}
+                            toggleFullScreen={toggleFullScreenDispatch}
+                            playing={playing}
+                            clickPlaying={togglePlayingDispatch}
+                        />
+                        <NormalPlayer
+                            song={currentSong}
+                            fullScreen={fullScreen}
+                            toggleFullScreen={toggleFullScreenDispatch}  
+                        />
+                    </>
+                )
+            }
+            <audio ref={audioRef}></audio>
         </div>
     );
 }
