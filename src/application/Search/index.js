deafefa31f4a2e36ce73bf580bba2cb29cf26ef0
plearner <953413537@qq.com>
2020-03-16 23:05:42 +0800

    完成搜索功能

--- src/application/Search/index.js
+++ src/application/Search/index.js
@@ -1,13 +1,51 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import  { CSSTransition } from 'react-transition-group';
-import { Container } from './style';
 import SearchBox from '../../baseUI/search-box';
+import { connect } from 'react-redux';
+import {
+    getHotKeyWords,
+    getSuggestList,
+    changeEnterLoading
+} from './store';
+import {
+    Container,
+    ShortcutWrapper,
+    HotKey,
+    List,
+    ListItem,
+    SongItem
+} from './style';
+import Scroll from '../../baseUI/scroll';
+import Loading from '../../baseUI/loading/index';
+import LazyLoad, { forceCheck } from 'react-lazyload';
+import { getName } from '../../api/utils';
+import singerPic from './singer.png';
+import musicPic from './music.png';
 
 function Search(props) {
+    const {
+        hotList, 
+        enterLoading, 
+        suggestList: immutableSuggestList, 
+        songsCount, 
+        songsList: immutableSongsList
+    } = props;
+    const suggestList = immutableSuggestList.toJS ();
+    const songsList = immutableSongsList.toJS ();
+    const {
+        getHotKeyWordsDispatch,
+        changeEnterLoadingDispatch,
+        getSuggestListDispatch,
+        getSongDetailDispatch
+    } = props;
+
     const [show, setShow] = useState(false);
     const [query, setQuery] = useState('');
-    useEffect(() => {
-        setShow(true);
+
+    useEffect (() => {
+        setShow (true);
+        if (!hotList.size)
+        getHotKeyWordsDispatch();
     }, []);
 
     const searchBack = useCallback(() => {
@@ -16,6 +54,94 @@ function Search(props) {
 
     const handleQuery = q => {
         setQuery(q);
+        if (!q) return;
+        changeEnterLoadingDispatch(true);
+        getSuggestListDispatch(q);
+    };
+    const renderHotKey = () => {
+        let list = hotList.toJS();
+        return (
+            <ul>
+                {
+                    list.map (item => {
+                        return (
+                            <li className="item" key={item.first} onClick={() => setQuery(item.first)}>
+                                <span>{item.first}</span>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    };
+
+    const renderSingers = () => {
+        return null;
+        let singers = suggestList.artists;
+        if (!singers || !singers.length) return null;
+        return (
+            <List>
+                <h1 className="title"> 相关歌手 </h1>
+                {
+                    singers.map((item, index) => {
+                        return (
+                            <ListItem key={item.accountId+""+index}>
+                                <div className="img_wrapper">
+                                    <LazyLoad placeholder={<img width="100%" height="100%" src={singerPic} alt="singer"/>}>
+                                        <img src={item.picUrl} width="100%" height="100%" alt="music"/>
+                                    </LazyLoad>
+                                </div>
+                                <span className="name"> 歌手: {item.name}</span>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    };
+
+    const renderAlbum = () => {
+        let albums = suggestList.playlists;
+        if (!albums || !albums.length) return null;
+        return (
+            <List>
+                <h1 className="title"> 相关歌单 </h1>
+                {
+                    albums.map((item, index) => {
+                        return (
+                            <ListItem key={item.accountId+""+index}>
+                                <div className="img_wrapper">
+                                    <LazyLoad placeholder={<img width="100%" height="100%" src={musicPic} alt="music"/>}>
+                                        <img src={item.coverImgUrl} width="100%" height="100%" alt="music"/>
+                                    </LazyLoad>
+                                </div>
+                                <span className="name"> 歌单: {item.name}</span>
+                            </ListItem>
+                        )
+                    })
+                }
+            </List>
+        )
+    };
+    const renderSongs = () => {
+        return (
+            <SongItem style={{paddingLeft: "20px"}}> 
+                {
+                    songsList.map (item => {
+                        return (
+                            <li key={item.id}>
+                                <div className="info">
+                                    <span>{item.name}</span>
+                                    <span>
+                                        {getName(item.artists)} - {item.album.name}
+                                    </span>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </SongItem>
+        )
     };
 
     return (
@@ -31,9 +157,49 @@ function Search(props) {
                 <div className="search_box_wrapper">
                     <SearchBox back={searchBack} newQuery={query} handleQuery={handleQuery}></SearchBox>
                 </div>
+                <ShortcutWrapper show={!query}>
+                    <Scroll>
+                        <div>
+                            <HotKey>
+                                <h1 className="title"> 热门搜索 </h1>
+                                {renderHotKey()}
+                            </HotKey>
+                        </div>
+                    </Scroll>
+                </ShortcutWrapper>
+                <ShortcutWrapper show={query}>
+                    <Scroll onScroll={forceCheck}>
+                        <div>
+                            {renderSingers()}
+                            {renderAlbum()}
+                            {renderSongs()}
+                        </div>
+                    </Scroll>
+                </ShortcutWrapper>
+                <Loading show={enterLoading}></Loading>
             </Container>
         </CSSTransition>
     );
 }
 
-export default Search;
+const mapStateToProps = (state) => ({
+    hotList: state.getIn(['search', 'hotList']),
+    enterLoading: state.getIn(['search', 'enterLoading']),
+    suggestList: state.getIn(['search', 'suggestList']),
+    songsCount: state.getIn(['player', 'playList']).size,
+    songsList: state.getIn(['search', 'songsList'])
+});
+
+const mapDispatchToProps = dispatch => ({
+    getHotKeyWordsDispatch() {
+        dispatch(getHotKeyWords());
+    },
+    changeEnterLoadingDispatch(data) {
+        dispatch(changeEnterLoading(data))
+    },
+    getSuggestListDispatch (data) {
+        dispatch(getSuggestList(data));
+    }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Search));
--- src/routes/index.js
+++ src/routes/index.js
@@ -56,7 +56,18 @@ export default [
                 exact: true,
                 key: 'search',
                 component: Search
+            },
+            {
+                path: "/album/:id",
+                exact: true,
+                key: "album",
+                component: Album
             }
         ]
     }
 ];
+
+/*
+真是学到了，从上面的路由配置来说，路由对应的页面，说到底也就是个组件，是组件就可以重复利用，可以
+被利用在路由、子路由，或者另外一个页面的路由，参数控制通过动态路由来传递
+*/
